90ccf58799401f5e478f331e24e504f91b1d7407
Kim Ouan <kim.ouan@web.de>
2020-10-23 20:59:47 +0200

    Close #11; Added Draft for the first classes

--- README.md
+++ README.md
@@ -21,3 +21,5 @@ Tan pronto como este proyecto se termine, habrá principalmente dos grupos de pe
 1. Como usario de ese aplicación podrá encontrar plantas nuevas y adecuadas y/o las condiciones que las necesitan.
 2. Como Vendedor de plantas puede actualizar su lista de productos (dependiendo de la mayoría de las solicitudes de búsqueda), puede ver en qué región hay muchas solicitudes (para la expansión de negocio) y/o anunciar
 
+[Aquí](utils/HU_classes.py) hay el primer borrador por unas clases.
+
---
+++ utils/HU_classes.py
@@ -0,0 +1,60 @@
+## Some Pseudocode as first draft
+import json
+from collections import OrderedDict
+database_dir = '/MyDatabaseDirectory'
+
+def save_json(obj, file, indent=4, sort_keys=True):
+    with open(file, 'w') as f:
+        json.dump(obj, f, sort_keys=sort_keys, indent=indent)
+
+class Request:
+	'''
+	User Request to add certain plants to database
+	'''
+	def __init__(self, Username, Emailadress, new_plant, description):
+		self.name = Username
+		self.mail = Emailadress
+		self.plant_name = new_plant
+		self.comment = description
+
+	def save(): 
+		json_dict = OrderedDict()
+		json_dict['Username'] = self.name
+		json_dict['User_email'] = self.mail
+		json_dict['plant_to_add'] = self.plant_name
+		json_dict['User_comment'] = self.comment
+		save_json(json_dict,database_dir)
+
+
+class Record_Traffic:
+	'''
+	Search Plants based on Room Condition and Location of User
+	'''
+	def __init__(self, location, conditions):
+		self.location = location
+		self.conditions = conditions
+	def save():
+		'''TODO:
+		add to statistics of application'''		
+
+
+class Record_Search:
+	'''
+	Search for conditions of certain specific plants
+	'''
+	def __init__(self, location, plant_name):
+		self.location = location
+		self.plant_name = plant_name
+	def save():
+		'''TODO
+		add to statistics of application'''
+
+class Search: 
+	def __init__(self,location, con1, con2, con3):
+		self.location = location
+		self.con1 = con1
+		self.con2 = con2
+		self.con3 = con3
+
+	def apply(): 
+		'''apply search algorithm''' 
