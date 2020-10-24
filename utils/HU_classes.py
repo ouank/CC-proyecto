11b6697b729352558b4cd89c27c1771965f10e33
Kim Ouan <kim.ouan@web.de>
2020-10-24 15:18:14 +0200

    created first tests

--- utils/HU_classes.py
+++ utils/HU_classes.py
@@ -1,6 +1,5 @@
 ## Some Pseudocode as first draft
 import json
-from collections import OrderedDict
 database_dir = '/MyDatabaseDirectory'
 
 def save_json(obj, file, indent=4, sort_keys=True):
@@ -11,14 +10,16 @@ class Request:
 	'''
 	User Request to add certain plants to database
 	'''
-	def __init__(self, Username, Emailadress, new_plant, description):
+	def __init__(self, new_plant, description=None, Username='NoName', Emailadress=None):
 		self.name = Username
 		self.mail = Emailadress
 		self.plant_name = new_plant
 		self.comment = description
+		
+		assert isinstance(self.plant_name, str), 'the plant name should be string'
 
 	def save(): 
-		json_dict = OrderedDict()
+		json_dict = dict()
 		json_dict['Username'] = self.name
 		json_dict['User_email'] = self.mail
 		json_dict['plant_to_add'] = self.plant_name
@@ -33,6 +34,8 @@ class Record_Traffic:
 	def __init__(self, location, conditions):
 		self.location = location
 		self.conditions = conditions
+
+		assert isinstance(self.location, dict), 'wrong content for location were given'
 	def save():
 		'''TODO:
 		add to statistics of application'''		
@@ -45,6 +48,8 @@ class Record_Search:
 	def __init__(self, location, plant_name):
 		self.location = location
 		self.plant_name = plant_name
+
+		assert isinstance(self.location, dict), 'wrong content for location were given'
 	def save():
 		'''TODO
 		add to statistics of application'''
