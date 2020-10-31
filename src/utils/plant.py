38285f3d34d320dcd783e75ff6d6f973fc2eeacf
Kim Ouan <kim.ouan@web.de>
2020-10-31 18:15:41 +0100

    created classes for Users and Plants

---
+++ src/utils/plant.py
@@ -0,0 +1,15 @@
+class Plant(object):
+	'''
+	[HU] As a plant lover, I would like to find plants with my own criteria
+	'''
+	def __init__(self, name: str, water_quantity: str, water_quality: str, care_quantity: str, wind: bool, brightness: str, location: dict)
+		self.name = name
+		self.w_qn = water_quantity
+		self.w_ql = water_quality
+		self.c_qn = care_quantity
+		self.wind = wind
+		self.bright = brightness
+		self.location = location
+
+	def search(self) -> list:
+		pass
---
+++ src/utils/usuario.py
@@ -0,0 +1,14 @@
+class User(object):
+	'''
+	This is the User class
+	'''
+	def __init__(self, name: str, location: dict, email: str)
+		self.name = name
+		self.location = location
+		self.email = email
+
+	def request(self, plant_name):
+		'''
+		[HU] As a User I want to make a suggestions about plants that should be added to the data base
+		'''
+		pass
