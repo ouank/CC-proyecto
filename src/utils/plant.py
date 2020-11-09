61f83f321457e5527c0f833476435d489589a496
Kim Ouan <kim.ouan@web.de>
2020-11-09 11:35:26 +0100

    added Description

--- src/utils/plant.py
+++ src/utils/plant.py
@@ -1,15 +1,28 @@
+from utils.user import User
+
 class Plant(object):
 	'''
 	[HU] As a plant lover, I would like to find plants with my own criteria
 	'''
-	def __init__(self, name: str, water_quantity: str, water_quality: str, care_quantity: str, wind: bool, brightness: str, location: dict) -> None:
+	def __init__(self, name: str, water_quantity: str, water_quality: str, care_quantity: str, wind: bool, brightness: str) -> None:
+		'''
+		Args:
+			name: name of the plant of type str
+			water_quantity: amount of water the plant to be searched will need. Out of [low, normal, high]
+			water_quality: what kind of water quality you can present to the plant. Out of [tap, rain]
+			care_quantity: how often you want to / can care for your plant [rarely, normal, often]
+			wind: If the chosen location in the room will be windy.
+			brightness: How bright will the location be? [shadow, partly, fully]
+		'''
 		self.name = name
 		self.w_qn = water_quantity
 		self.w_ql = water_quality
 		self.c_qn = care_quantity
 		self.wind = wind
 		self.bright = brightness
-		self.location = location
 
 	def search(self) -> list:
+		user_loc = User.get_location()
+		
+
 		pass
--- src/utils/user.py
+++ src/utils/user.py
@@ -2,13 +2,37 @@ class User(object):
 	'''
 	This is the User class 
 	'''
-	def __init__(self, name: str, location: dict, email: str) -> None:
+	def __init__(self, ID: int, name: str, location: dict, email: str) -> None:
+		self.id = ID
 		self.name = name
 		self.location = location
 		self.email = email
 
-	def request(self, plant_name):
+	def get_id(self):
+		return self.id
+
+	def get_name(self):
+		return self.name
+
+	def set_name(self, name):
+		self.name = name
+
+	def get_location(self):
+		return self.location
+
+	def get_email(self):
+		return self.email
+
+	def set_email(self,email):
+		self.email = email
+
+	def request(self, plant_name: str, website: str = None):
 		'''
 		[HU] As a User I want to make a suggestions about plants that should be added to the data base
+		Args:
+			plant_name: Name of the plant the user suggests to be added to the data bank
+			website: Optional, give a reference website for this plant 
 		'''
+		
+
 		pass
