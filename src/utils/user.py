e1a3817b8a1a4f585b42c13eaa4f2c13b8c2a6f2
Kim Ouan <kim.ouan@web.de>
2020-11-09 12:23:33 +0100

    modified request function

--- src/utils/user.py
+++ src/utils/user.py
@@ -1,3 +1,5 @@
+from .op_utils import *
+
 class User(object):
 	'''
 	This is the User class 
@@ -26,13 +28,25 @@ class User(object):
 	def set_email(self,email):
 		self.email = email
 
-	def request(self, plant_name: str, website: str = None):
+	def request(self, plant_name: str):
 		'''
 		[HU] As a User I want to make a suggestions about plants that should be added to the data base
+		For now it adds the plant to a simple list on the OS. The Saving point of the list will be changed in a later step
 		Args:
-			plant_name: Name of the plant the user suggests to be added to the data bank
-			website: Optional, give a reference website for this plant 
+			plant_name: Name of the plant the user suggests to be added to the data bank 
 		'''
-		
+		json_dict = load_json('new_plants.json')
+		user_id = str(self.id)
+		json_dict[user_id] = {}
+		#json_dict['User_ID'] = self.id
+		json_dict[user_id]['User_Name'] = self.name
+		json_dict[user_id]['User_location'] = self.location
+		json_dict[user_id]['User_email'] = self.email
+		json_dict[user_id]['new_plant'] = plant_name
+		print(json_dict)
+		save_json(json_dict, 'new_plants.json')
 
-		pass
+
+
+
+		#pass
