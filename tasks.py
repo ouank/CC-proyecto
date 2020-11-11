51f2482f8f56b4edfe5c0af1392cbcb8f4c16863
Kim Ouan <kim.ouan@web.de>
2020-11-11 17:22:13 +0100

    added task automation: create virtualenvironment for project

--- tasks.py
+++ tasks.py
@@ -1,5 +1,12 @@
 from invoke import task
 
+#Create Virtualenv
+@task
+def create(c):
+	print('Creating new virtual environment')
+	c.run('python3.8 -m venv selva_urbana')
+	c.run('source selva_urbana/bin/activate')
+
 # Install Requirements
 @task
 def install(c):
